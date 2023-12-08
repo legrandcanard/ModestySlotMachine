76e5743b7b247984bba628bf5e731f8d52634bdb
Vladislav Mironov <mironov.v.v@outlook.com>
2023-12-08 22:33:52 +0100

    Nav menu styles fixed

--- ModestySlotMachine/MauiProgram.cs
+++ ModestySlotMachine/MauiProgram.cs
@@ -24,8 +24,8 @@ namespace ModestySlotMachine
             builder.Services.AddMauiBlazorWebView();
 
 #if DEBUG
-		//builder.Services.AddBlazorWebViewDeveloperTools();
-		    builder.Logging.AddDebug();
+            builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Logging.AddDebug();
 #endif
 
             builder.Services.AddTransient<LibertyBellSlotService>();
